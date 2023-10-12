a40546bbe313faf4859fb2dc9c8b0143b9ccbef1
Dave Pagurek <davepagurek@gmail.com>
2023-10-12 10:07:27 -0400

    Some better scaffolding

--- optimize.py
+++ optimize.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import svgelements
 import math
 
-device = torch.device("mps") # TODO: change to "mps" for macbook
+device = torch.device("cpu") # TODO: support mps, cuda if available
 
 def load_obj(path):
     vertices = []
@@ -39,35 +39,8 @@ def load_svg(path, sample_rate=8, out_width=2, out_height=2):
                 lines.append([pts[i], pts[i+1]])
     return torch.Tensor(lines)
 
-# Calculates the signed distance from the edge v0-v1 to point p
-# https://gist.github.com/seece/7e2cc8f37b2446035c27cbc43a561ddc
-def signed_dist_to_edge(v0, v1, p):
-    """
-    let S = W*H
-    v0 and v1 have vertex positions for all N triangles.
-    Their shapes are [N x 2]
-    p is a list of sampling points as a [N x S x 2] tensor.
-    Each of the N triangles has an [S x 2] matrix of sampling points.
-    returns a [N x S] matrix
-    """
-
-    S = p.size()[1]
-
-    # Take all the x and y coordinates of all the positions as a
-    # [N x S] tensor
-    px = p[:, :, 0].cuda()
-    py = p[:, :, 1].cuda()
-
-    # We need to manually broadcast the vector to cover all sample points
-    y01 = v0[:,1] - v1[:,1] # [N]
-    x10 = v1[:,0] - v0[:,0] # [N]
-    y01 = y01.unsqueeze(0).t().repeat((1, S)).cuda() # [N x S]
-    x10 = x10.unsqueeze(0).t().repeat((1, S)).cuda() # [N x S]
-
-    cross = v0[:,0]*v1[:,1] - v0[:,1]*v1[:,0] # [N]
-    cross = cross.unsqueeze(0).t().repeat((1,S)) # [N x S]
-
-    return y01*px + x10*py + cross
+def softclamp(tensor, min=0, max=1, hardness=10):
+    return torch.special.expit((tensor - (max + min)/2) * hardness)
 
 class Mesh:
     def __init__(self, models):
@@ -77,7 +50,7 @@ class Mesh:
         self.rest_vertices = rest_vertices.to(device)
         self.vertices = rest_vertices.clone().to(device)
         # TODO make this a big tensor
-        self.blend_shapes = [(v[0] - rest_vertices).detach().to(device) for v in models[1:]]
+        self.blend_shapes = [(v[0] - rest_vertices).clone().detach().to(device) for v in models[1:]]
         self.blend_weights = torch.tensor([0.0 for v in self.blend_shapes], requires_grad=True, device=device)
         self.triangles = torch.zeros([len(self.faces), 3, 3]).to(device)
         self.update_triangles()
@@ -106,89 +79,131 @@ class Mesh:
     def cost(self, targets):
         pass
 
-    def display(self, target=None):
+    def render(self, target=None, iter=0):
+        alpha = self.brightness().cpu()
+
+        target_costs = torch.zeros_like(alpha)
+        target_weights = torch.zeros_like(alpha)
+        target_alphas = torch.zeros_like(alpha)
+
+        if target is not None:
+            for i in range(target.size(dim=0)):
+                alpha_cost, dist_weight, target_alpha = self.line_cost(alpha, i)
+                target_costs += alpha_cost / target.size(dim=0)
+                target_weights += dist_weight / target.size(dim=0)
+                target_alphas += target_alpha / target.size(dim=0)
+
         plt.figure()
-        to_camera = torch.Tensor([0, 0, 1]).cpu()
         for i in range(self.triangles.size(dim=0)):
-            v1 = self.triangles[i, 0, :].clone().detach().cpu()
-            v2 = self.triangles[i, 1, :].clone().detach().cpu()
-            v3 = self.triangles[i, 2, :].clone().detach().cpu()
-            t1 = v2 - v1
-            t2 = v3 - v1
-            n = torch.linalg.cross(t1, t2)
-            n = n / (torch.linalg.vector_norm(n) + 1e-10)
-            visibility = torch.dot(n, to_camera)
-            brightness = torch.clamp(visibility, min=0, max=1).pow(0.25).item()
-            alpha = (torch.clamp(visibility * 10, min=0, max=1) * (1 - brightness)).item()
-            color = (brightness, 0, 0, alpha)
-            triangle = plt.Polygon(self.triangles[i, :, 0:2].clone().detach().cpu(), color=color)
+            alpha_diff = target_alphas[i].item()-alpha[i].item()
+            color = (max(0, alpha_diff), 0, max(0, -alpha_diff), alpha[i].item() * target_weights[i].item())
+            triangle = plt.Polygon(self.triangles[i, :, 0:2].clone().detach().cpu(), color=color, ec='none')
             plt.gca().add_patch(triangle)
         plt.xlim(-2, 2)
         plt.ylim(-1, 1)
 
-        if (target is not None):
+        if target is not None:
             target_cpu = target.clone().detach().cpu()
             for line in range(target_cpu.size(dim=0)):
-                print(
-                    target_cpu[line,0,0].item(),
-                    target_cpu[line,0,1].item(),
-                    target_cpu[line,1,0].item(),
-                    target_cpu[line,1,1].item())
                 plt.plot(
                     (target_cpu[line,0,0].item(), target_cpu[line,1,0].item()),
                     (target_cpu[line,0,1].item(), target_cpu[line,1,1].item()),
                     'b'
                 )
-        plt.show()
+        plt.savefig(f"out/iter{iter}.png")
+        plt.close()
 
-    def render(self, img):
-        # TODO
-        w = img.size(dim=1)
-        h = img.size(dim=2)
-        grid_x, grid_y = torch.meshgrid(
-                torch.linspace(0, 1, width),
-                torch.linspace(0, 1, height))
-        coords = torch.dstack((grid_x, grid_y)) # [W x H x 2]
-        coords_list = torch.reshape(coords, (w*h, 2)) # [W*H x 2]
+    def brightness(self):
+        to_camera = torch.Tensor([0, 0, 1]).to(device)
+        t1 = self.triangles[:, 1, :] - self.triangles[:, 0, :]
+        t2 = self.triangles[:, 2, :] - self.triangles[:, 0, :]
+        normals = torch.linalg.cross(t1, t2)
+        normals = normals / (torch.unsqueeze(torch.linalg.vector_norm(normals, dim=-1), dim=1).expand(normals.size()) + 1e-10)
+        visibility = torch.linalg.vecdot(
+            normals,
+            to_camera.expand((normals.size(dim=0), 3))
+        )
+        brightness = torch.special.expit((visibility - 0.45) * 100)
+        alpha = torch.special.expit((visibility) * 100) * (1 - brightness)
+        return alpha
 
-        # Calculate the area of the parallelogram formed by the triangle
-        area = signed_dist_to_edge(self.triangles[:, 2, :], self.triangles[:, 1, :], self.triangles[:, None, 0, :])
+    def line_cost(self, alpha, i):
+        v1 = target[i, 0, :]
+        v2 = target[i, 1, :]
+        line_dir = v2 - v1
+        line_dir /= torch.linalg.norm(v2 - v1).expand(line_dir.size())
+        line_len = torch.squeeze(torch.linalg.vecdot(v2 - v1, line_dir))
+        line_normal = torch.stack([line_dir[1], -line_dir[0]], dim=-1)
 
-        # Evaluate the edge functions at every position.
-        # We should get a [N x P] vector out of each.
-        w0 = -signed_dist_to_edge(d[:, 1, :], d[:, 2, :], grid) / area
-        w1 = -signed_dist_to_edge(d[:, 2, :], d[:, 0, :], grid) / area
-        w2 = -signed_dist_to_edge(d[:, 0, :], d[:, 1, :], grid) / area
+        to_points = self.triangles[:, :, 0:2] - v1.expand(self.triangles[:, :, 0:2].size())
 
-        # Only pixels inside the triangles will have color
-        mask = (w0 > 0) & (w1 > 0) & (w2 > 0) # [N x P]
+        tangent_dists = torch.linalg.vecdot(to_points, line_dir.expand(to_points.size()))
+        tangent_min = torch.min(tangent_dists, dim=1)[0]
+        tangent_max = torch.max(tangent_dists, dim=1)[0]
+        min_tangent_costs = torch.pow(tangent_min - torch.min(torch.stack([torch.zeros_like(line_len), line_len])).expand(tangent_min.size()), 2)
+        max_tangent_costs = torch.pow(tangent_max - torch.max(torch.stack([torch.zeros_like(line_len), line_len])).expand(tangent_max.size()), 2)
 
+        normal_dists = torch.linalg.vecdot(to_points, line_normal.expand(to_points.size()))
+        min_normal = torch.min(normal_dists, dim=1)[0]
+        max_normal = torch.max(normal_dists, dim=1)[0]
+        min_normal_cost = torch.pow(min_normal, 2)
+        max_normal_cost = torch.pow(max_normal, 2)
 
+        # alignment_cost = max_normal - min_normal
+        normal_cost = min_normal_cost + max_normal_cost
+        tangent_cost = min_tangent_costs + max_tangent_costs
+
+        # sum_dist = torch.pow(normal_cost * 0.1, 2) + torch.pow(tangent_cost, 2)
+        # dist_weight = torch.pow(sum_dist * 0.01, -1)
+
+        min_dist = pow(torch.min(torch.stack([torch.abs(tangent_min), torch.abs(tangent_max)]), dim=0)[0], 2)
+        min_dist += pow(torch.min(torch.stack([torch.abs(min_normal), torch.abs(max_normal)]), dim=0)[0], 2)
+
+        alpha_dist_threshold = 1
+        target_alpha = -torch.special.expit((min_dist - alpha_dist_threshold**2) * 100) + 1
+        dist_cost = torch.special.expit((normal_cost + tangent_cost) * target_alpha * 10) * 100
+        print(dist_cost)
+        alpha_cost = torch.pow(alpha - target_alpha, 2) * dist_cost
+
+        dist_weight_threshold = 0.5
+        dist_weight = -torch.special.expit((min_dist - dist_weight_threshold**2) * 100) + 1
+        return (alpha_cost, dist_weight, target_alpha)
+
+    def loss(self, target):
+        loss = torch.tensor([0.0]).to(device)
+
+        alpha = self.brightness()
+
+        for i in range(target.size(dim=0)):
+            alpha_cost, dist_weight, target_alpha = self.line_cost(alpha, i)
+            loss += torch.sum(alpha_cost * dist_weight)
+
+        return loss
 
 suzanne = Mesh([
     load_obj("assets/suzanne0.obj"),
     load_obj("assets/suzanne1.obj"),
     load_obj("assets/suzanne2.obj")
 ])
-target = load_svg("assets/guides.svg", out_width=4, out_height=2)
+target = load_svg("assets/guides.svg", out_width=4, out_height=2).to(device)
 
-optimizer = torch.optim.Adam([suzanne.blend_weights], lr=1e-2)
+optimizer = torch.optim.Adam([suzanne.blend_weights], lr=2e-2) # 1e-2
 # Run 100 Adam iterations.
 for t in range(100):
     print('iteration:', t)
     optimizer.zero_grad()
     suzanne.update_triangles()
-    loss = torch.pow(abs(suzanne.triangles.sum()) + 1, -1)
+    loss = suzanne.loss(target)
 
     loss.backward()
 
     # Take a gradient descent step.
     optimizer.step()
     with torch.no_grad():
-        suzanne.constrain()
+        # suzanne.constrain()
+        suzanne.update_triangles()
     print(suzanne.blend_weights)
-    if t % 5 == 0:
-        suzanne.display(target)
+    suzanne.render(target, iter=t)
 
 suzanne.output('blended.obj')
 
